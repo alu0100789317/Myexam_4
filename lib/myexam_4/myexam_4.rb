4411b7ca8cbea7ea5c461b91080ebd4c3d129f5d
Jorge Machín <alu0100789317@ul.edu.es>
2014-11-11 20:00:49 +0000

    Terminando de crear las expectativas y finalizando la practica

--- lib/myexam_4/myexam_4.rb
+++ lib/myexam_4/myexam_4.rb
@@ -1,5 +1,50 @@
-require "myexam_4/version"
+class Preguntas
 
-module Myexam4
-  # Your code goes here...
+	include Comparable
+	
+	attr_accessor :pregunta
+
+	def initialize pregunta
+ 		@pregunta = pregunta
+	end
+end
+
+class SeleccionSimple < Preguntas
+
+	attr_accessor :respuestas
+
+	def initialize pregunta, respuestas
+ 		super(pregunta)
+		@respuestas = respuestas
+	end
+	
+	def <=>(other)
+		pregunta.size <=> other.pregunta.size
+	end
+
+	def ==(other)
+		pregunta == other.pregunta
+	end
+	
+	def to_s
+		"#{@pregunta}\n a:#{@respuestas[0]}\n b:#{@respuestas[1]}\n c:#{@respuestas[2]}\n d:#{@respuestas[3]}"
+	end 
+end
+
+class VerdaderoFalso < Preguntas
+
+	attr_accessor :respuestas
+	
+	def initialize pregunta
+		super(pregunta)
+		@respuestas = ["Verdadero","Falso"]
+	end
+
+	def <=>(other)
+		pregunta.size <=> other.pregunta.size
+	end	
+	
+	def to_s
+		"#{@pregunta}\n a: Verdadero \n b: Falso"
+	end
 end
